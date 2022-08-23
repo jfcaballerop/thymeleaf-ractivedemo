bbcb79517eacf7ad86d9c038a298a3dbdafa16f7
Jose Caballero <josefernando.cpastor@securitasdirect.es>
2022-08-23 15:50:45 +0200

    first commit

---
+++ src/main/java/com/mrknight/tutorial/thymeleafreactivedemo/ThymeleafReactivedemoApplication.java
@@ -0,0 +1,13 @@
+package com.mrknight.tutorial.thymeleafreactivedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafReactivedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafReactivedemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mrknight/tutorial/thymeleafreactivedemo/ThymeleafReactivedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrknight.tutorial.thymeleafreactivedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafReactivedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
